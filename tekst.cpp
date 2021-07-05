fa78c6244edc7b462f4ebb9faccde5d68f6a7288
Baran Usluel <me@baranusluel.com>
2021-07-05 19:21:49 +0300

    Fix scrolling into void bug by adding line memory to viewer

--- tekst.cpp
+++ tekst.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <curses.h>
 #include <iostream>
+#include <optional>
 #include <string>
 #include <vector>
 #include "Buffer.h"
@@ -21,29 +22,31 @@ std::ostringstream debugLog;
 #define LINES_TXT LINES - 2
 #define COLS_TXT COLS
 
-// Lookup table for lengths of lines displayed on screen.
-// Value of -1 indicates not an editable line.
-std::vector<size_t> lineLengths;
+// Table of lines / strings that are currently within the editor's view.
+// This is the viewer's text memory, not to be confused with Buffer's complete text memory.
+// Lines out of range have an empty optional.
+std::vector<std::optional<std::string>> linesInView;
 
 // Displays desired text line from file in target row.
 // Note that this method moves the cursor.
 void displayLine(WINDOW* win, int fileLineNum, int displayRow, Buffer* b) {
     // Get line from buffer in memory
-    std::optional<std::string> line = b->getLine(fileLineNum);
+    std::optional<std::string> line = b->getLine(fileLineNum);    
     if (line.has_value()) {
         /// TODO: Limit number of characters according to COLS?
-        // If this line being displayed in last row of text edit region, strip the
-        // newline or else curses will shift lines up and add blank line.
-        if (displayRow == LINES_TXT - 1 && line->length() > 0 && line->back() == '\n')
-            line->pop_back();
-        mvwaddstr(win, displayRow, 0, line->c_str());
-        size_t len = line->length();
-        if (len > 0 && line->back() == '\n') len--;
-        // Update line lengths table with newly loaded line (exclude newline character)
-        lineLengths.insert(lineLengths.begin() + displayRow, len);
-    } else {
-        lineLengths.insert(lineLengths.begin() + displayRow, -1);
+        // If this line is being displayed in last row of text edit region, strip
+        // the newline when printing or else curses will shift lines up and add blank line.
+        bool hasNewline = line->length() > 0 && line->back() == '\n';
+        if (displayRow == LINES_TXT - 1 && hasNewline) {
+            mvwaddstr(win, displayRow, 0,
+                line->substr(0, line->length() - 1).c_str());
+        } else {
+            mvwaddstr(win, displayRow, 0, line->c_str());
+        }
+        
     }
+    // Update memory with newly loaded line
+    linesInView.insert(linesInView.begin() + displayRow, line);
 }
 
 int main (int argc, char* argv[]) {
@@ -121,7 +124,7 @@ int main (int argc, char* argv[]) {
                 if (col == 0) {
                     // If not first line of text, then move to delete EOL of previous line
                     if (row > 0) {
-                        wmove(txtW, --row, colGoal = col = lineLengths[row - 1]);
+                        wmove(txtW, --row, colGoal = col = linesInView[row - 1]->length() - 1);
                     } else {
                         break;
                     }
@@ -134,13 +137,13 @@ int main (int argc, char* argv[]) {
                 b->delChar(row, col);
                 wdelch(txtW);
                 // If cursor is at end of line, deleting linebreak
-                if (col == lineLengths[row]) {
+                if (col == linesInView[row]->length() - 1) {
                     curs_set(0); // Hide cursor during operations to avoid flickering
                     wdeleteln(txtW); // Deletes next row and shifts everything up
-                    // Delete entry for current row from lineLengths, it will be readded by displayLine
-                    lineLengths.erase(lineLengths.begin() + row);
-                    // Delete entry for the deleted row from lineLengths
-                    lineLengths.erase(lineLengths.begin() + row);
+                    // Delete entry for current row from linesInView, it will be readded by displayLine
+                    linesInView.erase(linesInView.begin() + row);
+                    // Delete entry for the deleted row from linesInView
+                    linesInView.erase(linesInView.begin() + row);
                     // Display updated (concatenated) line
                     displayLine(txtW, scrollOffset + row, row, b.get());
                     // Display line that scrolled into view from bottom
@@ -148,14 +151,15 @@ int main (int argc, char* argv[]) {
                     wmove(txtW, row, col);
                     curs_set(1);
                 } else {
-                    lineLengths[row]--;
+                    // Delete char from line in view memory
+                    linesInView[row]->erase(col, 1);
                 }
                 break;
             case KEY_LEFT:
                 wmove(txtW, row, colGoal = col = std::max(col - 1, 0));
                 break;
             case KEY_RIGHT:
-                wmove(txtW, row, colGoal = col = std::min(std::min(col + 1, COLS_TXT - 1), (int)lineLengths[row]));
+                wmove(txtW, row, colGoal = col = std::min(std::min(col + 1, COLS_TXT - 1), (int)linesInView[row]->length() - 1));
                 break;
             case KEY_UP:
                 if (row == 0) {
@@ -164,41 +168,44 @@ int main (int argc, char* argv[]) {
                         wscrl(txtW, -1);
                         scrollOffset--;
                         // Delete entry from end of table because only tracking visible lines
-                        lineLengths.erase(lineLengths.end() - 1);
+                        linesInView.erase(linesInView.end() - 1);
                         // Load text to display in the new scrolled line.
-                        // This also adds an entry to the lineLengths table
                         displayLine(txtW, scrollOffset, row, b.get());
                     }
                 } else {
                     row--;
                 }
-                wmove(txtW, row, col = std::min(colGoal, (int)lineLengths[row]));
+                wmove(txtW, row, col = std::min(colGoal, (int)linesInView[row]->length() - 1));
                 curs_set(1);
                 break;
             case KEY_DOWN:
-                if (lineLengths[row + 1] == -1)
-                    break;
                 if (row == LINES_TXT - 1) {
+                    // If there is no accessible next line, don't move cursor down.
+                    // Can't check next line directly because not loaded into view memory yet,
+                    // so instead check if there is newline at the end of this line.
+                    if (linesInView[row]->back() != '\n')
+                        break;
                     curs_set(0); // Hide cursor during operations to avoid flickering
                     wscrl(txtW, 1);
                     scrollOffset++;
                     // Delete entry from start of table because only tracking visible lines
-                    lineLengths.erase(lineLengths.begin());
+                    linesInView.erase(linesInView.begin());
                     // Load text to display in the new scrolled line.
-                    // This also adds an entry to the lineLengths table
                     displayLine(txtW, LINES_TXT - 1 + scrollOffset, row, b.get());
                 } else {
+                    // If there is no accessible next line, don't move cursor down
+                    if (!linesInView[row + 1].has_value())
+                        break;
                     row++;
                 }
-                /// TODO: Shouldn't be able to go down into empty space
-                wmove(txtW, row, col = std::min(colGoal, (int)lineLengths[row]));
+                wmove(txtW, row, col = std::min(colGoal, (int)linesInView[row]->length() - 1));
                 curs_set(1);
                 break;
             case KEY_HOME:
                 wmove(txtW, row, colGoal = col = 0);
                 break;
             case KEY_END:
-                wmove(txtW, row, colGoal = col = lineLengths[row]);
+                wmove(txtW, row, colGoal = col = linesInView[row]->length() - 1);
                 break;
             case ctrl('s'):
                 try {
@@ -214,20 +221,21 @@ int main (int argc, char* argv[]) {
                 b->insertChar(ch, row, col); // Insert character in buffer
                 if (ch == '\n') {
                     curs_set(0); // Hide cursor during operations to avoid flickering
-                    // New length of current line is wherever linebreak was added
-                    lineLengths[row] = col;
+                    // Current line is truncated to linebreak position in view memory
+                    linesInView[row] = linesInView[row]->substr(0, col);
                     // Move cursor to start of next row
                     wmove(txtW, ++row, colGoal = col = 0);
                     winsertln(txtW); // Add new empty line on screen above cursor, shifting rest down
                     displayLine(txtW, scrollOffset + row, row, b.get()); // Text to go in new line
                     wmove(txtW, row, col);
                     // Delete entry from end of table because only tracking visible lines
-                    lineLengths.erase(lineLengths.end() - 1);
+                    linesInView.erase(linesInView.end() - 1);
                     curs_set(1);
                 } else {
+                    // Insert new character into line in view memory
+                    linesInView[row]->insert(linesInView[row]->begin() + col, ch);
                     // Move cursor right when character typed
                     wmove(txtW, row, colGoal = col = std::min(col + 1, COLS_TXT - 1));
-                    lineLengths[row]++;
                 }
                 
         }
